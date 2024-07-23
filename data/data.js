b7817c595f504fd5a2de441637c730bcce5bd3fd
Ar4sCode <ar4sazizi@gmail.com>
2024-07-23 15:19:44 +0330

    Add files to Start and Add products data in data.js

---
+++ data/data.js
@@ -0,0 +1,56 @@
+export const products = [
+  {
+    name: "Waffle with Berries",
+    title: "Waffle",
+    priceCents: 650,
+    img: "../assets/images/image-waffle-thumbnail.jpg.jpg",
+  },
+  {
+    name: "Vanilla bean Creme Brulee",
+    title: "Creme Brulee",
+    priceCents: 700,
+    img: "../assets/images/image-creme-brulee-thumbnail.jpg",
+  },
+  {
+    name: "Macaroon Mix of Five",
+    title: "Macaroon",
+    priceCents: 800,
+    img: "../assets/images/image-macaron-thumbnail.jpg",
+  },
+  {
+    name: "Classic Tiramisu",
+    title: "Tiramisu",
+    priceCents: 550,
+    img: "../assets/images/image-tiramisu-thumbnail.jpg",
+  },
+  {
+    name: "Pistachio Baklava",
+    title: "Baklava",
+    priceCents: 400,
+    img: "../assets/images/image-baklava-thumbnail.jpg",
+  },
+  {
+    name: "Lemon Meringue Pie",
+    title: "Pie",
+    priceCents: 500,
+    img: "../assets/images/image-meringue-thumbnail.jpg",
+  },
+  {
+    name: "Red Velvet Cake",
+    title: "Cake",
+    priceCents: 450,
+    img: "../assets/images/image-cake-thumbnail.jpg",
+  },
+  {
+    name: "Salted Caramel Brownie",
+    title: "Brownie",
+    priceCents: 450,
+    img: "../assets/images/image-brownie-thumbnail.jpg",
+  },
+  {
+    name: "Vanilla Panna Cotta",
+    title: "Panna Cotta",
+    priceCents: 650,
+    img: "../assets/images/image-panna-cotta-thumbnail.jpg",
+  },
+];
