5210497c59f07b9abb1c191c7c95939a50ff29c9
Ar4sCode <ar4sazizi@gmail.com>
2024-07-25 12:02:58 +0330

    Create data folder for data.json and Generate HTML with js

---
+++ scripts/product.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+fetch("../data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // Use the JSON data in your project
+    let generateHTML = "";
+
+    data.forEach((product) => {
+      generateHTML += `<div class="product-container">
+          <div class="thumbnail-place">
+            <img class="thumbnail-img" src="${product.image.thumbnail}" alt="Waffle">
+            <div class="add-to-cart">
+              <img src="./assets/images/icon-add-to-cart.svg" alt="">
+              Add to Cart
+            </div>
+          </div>
+          <div class="product-detail">
+            <p class="product-title">${product.category}</p>
+            <p class="product-name">${product.name}</p>
+            <p class="product-price">$${product.price}</p>
+          </div>
+        </div>`;
+    });
+
+    document.querySelector(".container").innerHTML = generateHTML;
+  })
+  .catch((error) => console.error("Error reading the JSON file:", error));
